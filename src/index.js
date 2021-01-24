52d9b277180f96be8309e8395a15f73c97da185c
Roger Takeshita <roger.takeshita@gmail.com>
2021-01-24 14:02:27 -0500

    feat: Should update structure

--- src/index.js
+++ src/index.js
@@ -6,70 +6,54 @@ const fs = require('fs');
 const os = require('os');
 const path = require('path');
 const execSync = require('child_process').execSync;
+
 const gitconfig = path.join(os.homedir(), '.gitconfig');
 const gitignoreGlobal = path.join(os.homedir(), '.gitignore_global');
 const gitconfigExists = fs.existsSync(gitconfig);
 
-const createFolder = (newFolderPath) => {
-    if (!fs.existsSync(newFolderPath)) {
-        fs.mkdirSync(newFolderPath);
+const getGitHubAccounts = () => {
+    if (!gitconfigExists) {
+        console.log(`${gitconfig} not found. Please create one and try again`);
     } else {
-        console.log('Folder already exists');
-        process.exit(0);
-    }
-};
-
-const copyGitignore = (gitignoreGlobal, newFolderPath) => {
-    console.log('——————›Copying .gitignore_global into the repo...');
-    if (fs.existsSync(gitignoreGlobal)) {
-        fs.copyFileSync(gitignoreGlobal, `${newFolderPath}/.gitignore`);
-        console.log('   Done!');
-    } else {
-        console.log(
-            `Looks like you don't have ${gitignoreGlobal}. The process will continue without creating one.`
+        const gitconfigAccounts = execSync(
+            'git config --get-regex user[0-9]*.acc',
+            {
+                encoding: 'utf-8',
+            }
         );
-    }
-};
+        const re = /(user([0-9]?)*)/gim;
+        const accArray = gitconfigAccounts.match(re);
 
-const createREADME = (answers) => {
-    const text = `<h1 id='contents'>Table of Contents</h1>\n\n- [${answers.repositoryName.toUpperCase()}](#${answers.repositoryName
-        .trim()
-        .replace(
-            /\s+/g,
-            '-'
-        )})\n\n# ${answers.repositoryName.toUpperCase()}\n\n[Go Back to Contents](#contents)\n\n`;
-    fs.writeFileSync('README.md', text);
-};
+        return accArray.map((profile) => {
+            let acc = '';
+            let token = '';
 
-const createRepository = async (answers, newFolderName, accounts) => {
-    execSync('git init; git add .; git commit -m "First Commit"');
-    execSync('git branch -M main');
-    execSync(
-        `git remote add origin https://github.com/${answers.acc}/${newFolderName}.git`
-    );
+            try {
+                acc = execSync(`git config ${profile}.acc`, {
+                    encoding: 'utf-8',
+                }).replace('\n', '');
+                token = execSync(`git config ${profile}.token`, {
+                    encoding: 'utf-8',
+                }).replace('\n', '');
+            } catch (error) {}
 
-    const profile = accounts.find((prof) => prof.acc === answers.acc);
-    const octokit = new Octokit({ auth: profile.token });
-
-    try {
-        return await octokit.request('POST /user/repos', {
-            name: newFolderName,
-            private: answers.private,
+            return {
+                acc,
+                token,
+                profile,
+            };
         });
-    } catch (error) {
-        throw new Error(error);
     }
 };
 
-const buildConfig = async (accounts) => {
-    const accountsName = accounts.map((profile) => profile.acc);
-    const answers = await inquirer.prompt([
+const askQuestions = async (accountsName, accObjArray) => {
+    return inquirer.prompt([
         {
             type: 'list',
             name: 'acc',
             message: 'What GitHub account do you want to use?',
             choices: accountsName,
-            when: () => accounts.length > 1,
+            when: () => accObjArray.length > 1,
         },
         {
             type: 'text',
@@ -85,52 +69,91 @@ const buildConfig = async (accounts) => {
             default: 'true',
         },
     ]);
+};
+
+const createCDFolder = (newFolderPath) => {
+    if (!fs.existsSync(newFolderPath)) {
+        fs.mkdirSync(newFolderPath);
+        process.chdir(newFolderPath);
+    } else {
+        console.log('Folder already exists, process aborted!');
+        process.exit(0);
+    }
+};
+
+const copyGitignore = (gitignoreGlobal) => {
+    console.log('——————› Copying .gitignore_global...');
+    if (fs.existsSync(gitignoreGlobal)) {
+        fs.copyFileSync(gitignoreGlobal, `.gitignore`);
+        console.log('   Done!');
+    } else {
+        console.log(
+            `Looks like you don't have ${gitignoreGlobal}. The process will continue without creating one.`
+        );
+    }
+};
+
+const createREADME = (answers) => {
+    console.log('——————› Creating README.md...');
+    const text = `<h1 id='contents'>Table of Contents</h1>\n\n- [${answers.repositoryName.toUpperCase()}](#${answers.repositoryName
+        .trim()
+        .replace(
+            /\s+/g,
+            '-'
+        )})\n\n# ${answers.repositoryName.toUpperCase()}\n\n[Go Back to Contents](#contents)\n\n`;
+    fs.writeFileSync('README.md', text);
+};
+
+const createRemoteRepo = async (answers, newFolderName, accObjArray) => {
+    console.log('——————› Creating GitHub repository...');
+    const profile = accObjArray.find((prof) => prof.acc === answers.acc);
+    const octokit = new Octokit({ auth: profile.token });
+
+    try {
+        return await octokit.request('POST /user/repos', {
+            name: newFolderName,
+            private: answers.private,
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+const pushFirstCommit = (answers, newFolderName) => {
+    console.log('——————› Pushing first commit...');
+    execSync('git init; git add .; git commit -m "First Commit"');
+    execSync('git branch -M main');
+    execSync(
+        `git remote add origin https://github.com/${answers.acc}/${newFolderName}.git`
+    );
+    execSync('git push -u origin main');
+};
+
+const createRepo = async () => {
+    const accObjArray = getGitHubAccounts();
+    const accountsName = accObjArray.map((profile) => profile.acc);
+    const answers = await askQuestions(accountsName, accObjArray);
 
     if (!answers.hasOwnProperty('acc')) {
-        answers.acc = accounts[0].acc;
+        answers.acc = accObjArray[0].acc;
     }
 
     const newFolderName = answers.repositoryName.trim().replace(/\s+/g, '_');
     const newFolderPath = path.join(process.cwd(), newFolderName);
-    createFolder(newFolderPath);
-    process.chdir(newFolderPath);
+
+    createCDFolder(newFolderPath);
     copyGitignore(gitignoreGlobal, newFolderPath);
     createREADME(answers);
 
     try {
-        await createRepository(answers, newFolderName, accounts);
-        execSync('git push -u origin main');
+        await createRemoteRepo(answers, newFolderName, accObjArray);
+        pushFirstCommit(answers, newFolderName);
         console.log('All Done!');
     } catch (error) {
         console.log(error);
     }
+
     process.exit(0);
 };
 
-if (!gitconfigExists) {
-    console.log(`${gitconfig} not found. Please create one and try again`);
-} else {
-    const config = execSync('git config --get-regex user[0-9]*.acc', {
-        encoding: 'utf-8',
-    });
-    const re = /(user([0-9]?)*)/gim;
-    const accountsArray = config.match(re);
-    const accountsObj = accountsArray.map((profile) => {
-        let acc = '';
-        let token = '';
-        try {
-            acc = execSync(`git config ${profile}.acc`, {
-                encoding: 'utf-8',
-            }).replace('\n', '');
-            token = execSync(`git config ${profile}.token`, {
-                encoding: 'utf-8',
-            }).replace('\n', '');
-        } catch (error) {}
-        return {
-            acc,
-            token,
-            profile,
-        };
-    });
-    buildConfig(accountsObj);
-}
+createRepo();
